18a1ac1ce8ec7ff839e1311415266ac484a9d93a
HuskyDG <84650617+HuskyDG@users.noreply.github.com>
2025-01-15 12:20:36 +0700

    Fix finding the sender name

--- aichat.py
+++ aichat.py
@@ -122,6 +122,7 @@ try:
                 #print(chat_btn.text)
                 try:
                     chat_btn.find_element(By.CSS_SELECTOR, 'span[class="x6s0dn4 xzolkzo x12go9s9 x1rnf11y xprq8jg x9f619 x3nfvp2 xl56j7k x1spa7qu x1kpxq89 xsmyaan"]')
+                    who_chatted = chat_btn.find_element(By.CSS_SELECTOR, 'span[class="x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft"]').text
                 except Exception:
                     continue
                 
@@ -166,7 +167,7 @@ Note:
 - To make the conversation less boring, you can ask the other person some interesting questions.
 - IMPORTANT! The content you create for me is the content of the reply message.
 
-The Messenger conversation is as follows:
+The Messenger conversation with "{who_chatted}" is as follows:
 
 """
 
@@ -174,13 +175,20 @@ The Messenger conversation is as follows:
                     try:
                         timedate = msg_element.find_element(By.CSS_SELECTOR, 'span[class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x676frb x1pg5gke xvq8zen xo1l8bm x12scifz"]')
                         prompt += "\n--- " + timedate.text + " ---"
-                        print("--- " + timedate.text + " ---")
                     except NoSuchElementException:
                         pass
+                        
+                    # Finding name
                     try: 
-                        name = "- " + msg_element.find_element(By.CSS_SELECTOR, 'h4[dir="auto"][class^="html-h4 "]').text
+                        name = "- " + msg_element.find_element(By.CSS_SELECTOR, 'h4').text
                     except NoSuchElementException:
                         name = None
+                    if name == None:
+                        try: 
+                            name = "- " + msg_element.find_element(By.CSS_SELECTOR, 'span[class="html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs xzpqnlu x1hyvwdk xjm9jq1 x6ikm8r x10wlt62 x10l6tqk x1i1rx1s"]').text
+                        except NoSuchElementException:
+                            name = None
+                    
                     msg = None
                     try:
                         msg = msg_element.find_element(By.CSS_SELECTOR, 'div[dir="auto"][class^="html-div "]').text
@@ -218,7 +226,6 @@ The Messenger conversation is as follows:
                         info_msg = "  " + msg
                     else:
                         info_msg = name + ": " + msg
-                    print(info_msg)
                     prompt += "\n" + info_msg
 
                 button = driver.find_element(By.CSS_SELECTOR, 'p[class="xat24cr xdj266r"]')
@@ -227,6 +234,8 @@ The Messenger conversation is as follows:
                 driver.execute_script("arguments[0].click();", button)
                 time.sleep(2)
                 button.send_keys(remove_non_bmp_characters(caption + "\n"))
+                
+                print(prompt)
                 print("AI Trả lời:", caption)
                 time.sleep(2)
                 
@@ -240,3 +249,4 @@ The Messenger conversation is as follows:
 
 finally:
     driver.quit()
+    

327ee2566a0b54cf5216652200240f46612a0f3e
shìwēi nguyen <84650617+HuskyDG@users.noreply.github.com>
2025-01-16 20:21:52 +0700

    except Exception

--- aichat.py
+++ aichat.py
@@ -226,31 +226,31 @@ The Messenger conversation with "{who_chatted}" is as follows:
                     try:
                         timedate = msg_element.find_element(By.CSS_SELECTOR, 'span[class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x676frb x1pg5gke xvq8zen xo1l8bm x12scifz"]')
                         prompt += "\n--- " + timedate.text + " ---"
-                    except NoSuchElementException:
+                    except Exception:
                         pass
                         
                     # Finding name
                     try: 
                         msg_element.find_element(By.CSS_SELECTOR, 'div[class="html-div xexx8yu x4uap5 x18d9i69 xkhd6sd x1gslohp x11i5rnm x12nagc x1mh8g0r x1yc453h x126k92a xyk4ms5"]').text
                         name = "Tin nhắn của bạn"
-                    except NoSuchElementException:
+                    except Exception:
                         name = None
 
                     if name == None:
                         try: 
                             name = "- " + msg_element.find_element(By.CSS_SELECTOR, 'h4').text
-                        except NoSuchElementException:
+                        except Exception:
                             name = None
                     if name == None:
                         try: 
                             name = "- " + msg_element.find_element(By.CSS_SELECTOR, 'span[class="html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs xzpqnlu x1hyvwdk xjm9jq1 x6ikm8r x10wlt62 x10l6tqk x1i1rx1s"]').text
-                        except NoSuchElementException:
+                        except Exception:
                             name = None
                     
                     msg = None
                     try:
                         msg = msg_element.find_element(By.CSS_SELECTOR, 'div[dir="auto"][class^="html-div "]').text
-                    except NoSuchElementException:
+                    except Exception:
                         pass
                     
                     try:

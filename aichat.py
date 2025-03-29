f2423b46ba0743f75443f2fec499b6ab22bd31a4
HuskyDG <84650617+HuskyDG@users.noreply.github.com>
2025-03-29 16:25:01 +0700

    refactor: scraper name of user

--- aichat.py
+++ aichat.py
@@ -723,27 +723,16 @@ try:
                                     except Exception:
                                         name = None
                                         mark = "text_message"
-
-                                    if name == None:
-                                        try: 
-                                            name = msg_element.find_element(By.CSS_SELECTOR, 'img[class="x1rg5ohu x5yr21d xl1xv1r xh8yej3"]').get_attribute("alt")
-                                            name =  f"{who_chatted} ({name})"
-                                        except Exception:
-                                            name = None
-
-                                    if name == None:
-                                        try: 
-                                            name = msg_element.find_element(By.CSS_SELECTOR, 'h4').text
-                                            name =  f"{who_chatted} ({name})"
-                                        except Exception:
-                                            name = None
-                                    if name == None:
-                                        try: 
-                                            name = msg_element.find_element(By.CSS_SELECTOR, 'span[class="html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs xzpqnlu x1hyvwdk xjm9jq1 x6ikm8r x10wlt62 x10l6tqk x1i1rx1s"]').text
-                                            name =  f"{who_chatted} ({name})"
-                                        except Exception:
-                                            name = None
-                                    
+                                    # Scrape user name
+                                    name = None
+                                    selectors = ['h4', 'h5[dir="auto"]', 'span.html-span']
+                                    for selector in selectors:
+                                        if not name:
+                                            try:
+                                                name = msg_element.find_element(By.CSS_SELECTOR, selector).text
+                                            except Exception:
+                                                name = None
+                                    # Scrape message
                                     msg = None
                                     try:
                                         msg_frame = msg_element.find_element(By.CSS_SELECTOR, 'div[dir="auto"][class^="html-div "]')
